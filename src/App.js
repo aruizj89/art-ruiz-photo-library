054318ebe76863b27aba22aea8648bf73637d582
Art Ruiz <a.ruiz.jr@gmail.com>
2022-05-15 08:36:30 -0500

    Reorganized ImageList component. Updated ImageList component to have a 2 column list in smaller viewports.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { Container, ImageList, ImageListItem } from "@mui/material";
+import { Container } from "@mui/material";
 import React from "react";
-import { LazyImage } from "./components/Image";
+import { ImageList } from "./components/ImageList";
 import photos from "./photos.json";
 
 const App = () => {
   return (
     <Container sx={{ pt: 3 }}>
-      <ImageList cols={3} gap={24} sx={{ m: 0 }} variant="masonry">
-        {photos.map((photo) => (
-          <ImageListItem key={photo.id} sx={{ width: "100%" }}>
-            <LazyImage photo={photo} />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <ImageList photos={photos} />
     </Container>
   );
 };
