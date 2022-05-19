647ac862a207b49b0a3a36e71a3d7f8ca9453e2a
Art Ruiz <a.ruiz.jr@gmail.com>
2022-05-19 00:41:45 -0500

    Updated photos to be a dynamic fetch from custom api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,26 @@
 import { Container } from "@mui/material";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { ImageList } from "./components/ImageList";
-import photos from "./photos.json";
+import axios from "axios";
 
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  const fetch = useCallback(
+    () =>
+      axios
+        .post(
+          "https://us-central1-art-ruiz-photo-library.cloudfunctions.net/api/unsplash/random",
+          { count: 20 }
+        )
+        .then(({ data }) => setPhotos(data)),
+    []
+  );
+
+  useEffect(() => {
+    if (fetch) fetch();
+  }, [fetch]);
+
   return (
     <Container sx={{ pt: 3 }}>
       <ImageList photos={photos} />
